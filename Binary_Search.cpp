c51c747f5d30c14e3adf4c0574874164f9bb59a5
Aditi kumari <150132755+Aditi139@users.noreply.github.com>
2026-01-27 10:05:59 +0530

    Implement binary search algorithm in C++

---
+++ Binary_Search.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 9;
+
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            cout << mid;
+            return 0;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    cout << -1;
+    return 0;
+}
