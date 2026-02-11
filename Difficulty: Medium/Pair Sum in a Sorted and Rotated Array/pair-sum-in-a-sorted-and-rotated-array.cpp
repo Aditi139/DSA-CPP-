90f6da112cd62ab8c23ec2b512f151db9ac61f25
Aditi kumari <150132755+Aditi139@users.noreply.github.com>
2026-02-12 00:26:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pair Sum in a Sorted and Rotated Array/pair-sum-in-a-sorted-and-rotated-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    bool pairInSortedRotated(vector<int>& arr, int target) {
+        int n = arr.size();
+        
+        int pivot = -1;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+        
+        if (pivot == -1) pivot = n - 1;
+        
+        int l = (pivot + 1) % n;
+        int r = pivot;
+        
+        while (l != r) {
+            int sum = arr[l] + arr[r];
+            
+            if (sum == target) return true;
+            else if (sum < target)
+                l = (l + 1) % n;
+            else
+                r = (n + r - 1) % n;
+        }
+        
+        return false;
+    }
+};
