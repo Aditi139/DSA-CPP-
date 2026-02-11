d9a6830552b527545983028d46aca6c35e72e9c8
Aditi kumari <150132755+Aditi139@users.noreply.github.com>
2026-02-12 00:15:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+         int n = arr.size();
+        int k = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[k++] = arr[i];
+            }
+        }
+
+        while (k < n) {
+            arr[k++] = 0;
+    }
+    }
+};
