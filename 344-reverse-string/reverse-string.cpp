6410a45fcc2bb5721a3de4cff022f17ce052f004
Aditi kumari <150132755+Aditi139@users.noreply.github.com>
2026-04-30 20:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (50.85%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(), s.end());
+        // reverse(s.begin(), s.end());
+       
+   
+
+    // method 2
+       int i = 0, j = s.size() - 1;
+        
+        while(i <= j){
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+
+//    method 3
+//    int n = s.size();
+        
+//         for(int i = 0; i < n/2; i++){
+//             swap(s[i], s[n - i - 1]);
+//         }
+
     }
 };
