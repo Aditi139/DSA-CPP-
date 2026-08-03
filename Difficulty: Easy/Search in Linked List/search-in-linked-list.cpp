0be1b9f6300f35cb683948c104cebc16f87397c6
Aditi kumari <150132755+Aditi139@users.noreply.github.com>
2026-08-03 11:21:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in Linked List/search-in-linked-list.cpp
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.cpp
@@ -14,14 +14,22 @@ class Solution {
   public:
     bool searchKey(Node* head, int key) {
         // Code here
-        Node* temp = head;
-        while(temp != nullptr){
-            if(temp->data == key){
-                return true;
-            } 
-                 temp=temp->next;
+        // Node* temp = head;
+        // while(temp!= nullptr){
+        //     if(temp->data == key){
+        //         return true; }
+        //         temp = temp->next;
             
+        // }
+        //     return false;
+        
+        while(head != nullptr){
+            if(head->data == key){
+                return true;
+            }
+            head= head->next;
         }
         return false;
+        
     }
 };
